d76f0581d6a47fd1fd1407097d10c596c1005539
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-06-04 14:00:35 -0400

    set up adapter for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
 
 export default config;
