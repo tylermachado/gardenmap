74cd6649a4b96350a17438623b15a97ffb43fb81
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-06-04 14:01:18 -0400

    set up adapter for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -7,10 +7,13 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: '404.html',
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
