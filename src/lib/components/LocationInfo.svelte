dafcdcadf182a9eb3c63037a8e435889598e2eec
tylermachado <3586466+tylermachado@users.noreply.github.com>
2026-04-26 14:22:53 -0400

    adjust styles of infoboxes

--- src/lib/components/LocationInfo.svelte
+++ src/lib/components/LocationInfo.svelte
@@ -43,7 +43,7 @@
 			{#if pointLayerData.phz}
 				<div class="rounded border border-stone-500 bg-stone-100 p-2">
 					<h4 class="font-semibold text-xs tracking-wide">USDA 2023 Plant Hardiness Zone</h4>
-					<ul class="mt-1 text-[11px] leading-tight">
+					<ul class="mt-1 text-[11px] leading-tight space-y-1">
 						{#if pointLayerData.phz.zone}
 							<li class="font-mono"><span class="text-stone-700">USDA 2023 Plant Hardiness Zone</span>: {pointLayerData.phz.zone}</li>
 						{/if}
@@ -61,10 +61,12 @@
 				<div class="rounded border border-stone-500 bg-stone-100 p-2">
 					<h4 class="font-semibold text-xs tracking-wide">North American Ecoregions - Level III</h4>
 					{#if pointLayerData.ecoregions.NA_L3NAME}
-						<p class="mt-1 text-[11px] leading-tight font-mono"><span class="text-stone-700">Level 3</span>: {toTitleCase(pointLayerData.ecoregions.NA_L3NAME)}</p>
-						<p class="mt-1 text-[11px] leading-tight font-mono"><span class="text-stone-700">Level 2</span>: {toTitleCase(pointLayerData.ecoregions.NA_L2NAME)}</p>
-						<p class="mt-1 text-[11px] leading-tight font-mono"><span class="text-stone-700">Level 1</span>: {toTitleCase(pointLayerData.ecoregions.NA_L1NAME)}</p>
-						<p class="mt-1 text-[11px] leading-tight"><span class="font-mono text-stone-700"><a href="https://sgi-gardenlibrary.maps.arcgis.com/sharing/rest/content/items/79bca4b771a04cb0b61176cf6f778565/data" target="_blank" rel="noopener noreferrer" class="underline">View detailed Ecoregion Descriptions</a></span></p>
+						<ul class="mt-1 text-[11px] leading-tight space-y-1">
+							<li class="font-mono"><span class="text-stone-700">Level 3</span>: {toTitleCase(pointLayerData.ecoregions.NA_L3NAME)}</li>
+							<li class="font-mono"><span class="text-stone-700">Level 2</span>: {toTitleCase(pointLayerData.ecoregions.NA_L2NAME)}</li>
+							<li class="font-mono"><span class="text-stone-700">Level 1</span>: {toTitleCase(pointLayerData.ecoregions.NA_L1NAME)}</li>
+							<li class="font-mono"><span class="text-stone-700"><a href="https://sgi-gardenlibrary.maps.arcgis.com/sharing/rest/content/items/79bca4b771a04cb0b61176cf6f778565/data" target="_blank" rel="noopener noreferrer" class="underline">View detailed Ecoregion Descriptions</a></span></li>
+						</ul>
 					{/if}
 				</div>
 			{/if}
